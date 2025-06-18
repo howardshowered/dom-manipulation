6d102d2a1383ef8cb6d0f0fc7b64bdc8ac42ed8b
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-06-17 17:45:42 -0700

    practiced dom manipulation

--- js-file.js
+++ js-file.js
@@ -5,3 +5,30 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content";
 
 container.appendChild(content);
+
+const paragraphNode = document.createElement("p");
+paragraphNode.textContent = "Hey I'm red!";
+paragraphNode.style.color = "red";
+
+container.appendChild(paragraphNode);
+
+const hThreeNode = document.createElement("h3");
+hThreeNode.textContent = "I'm a blue h3!";
+hThreeNode.style.color = "blue";
+
+container.appendChild(hThreeNode);
+
+const divNode = document.createElement("div");
+divNode.style.borderColor = "black";
+divNode.style.backgroundColor = "pink";
+
+const anotherHeading = document.createElement("h1");
+anotherHeading.textContent = "I'm in a div";
+
+const anotherParagraph = document.createElement("p");
+anotherParagraph.textContent = "ME TOO!";
+divNode.appendChild(anotherHeading);
+divNode.appendChild(anotherParagraph);
+
+container.appendChild(divNode);
+
